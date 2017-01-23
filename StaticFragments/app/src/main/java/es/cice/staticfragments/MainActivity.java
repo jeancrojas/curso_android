c2b380127d6df3fa5b327d87e9edbeacd2359fa0
cice <a@b.es>
2017-01-23 13:54:20 +0100

    23 de Enero de 2017 v2

--- StaticFragments/app/src/main/java/es/cice/staticfragments/MainActivity.java
+++ StaticFragments/app/src/main/java/es/cice/staticfragments/MainActivity.java
@@ -1,15 +1,20 @@
 package es.cice.staticfragments;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import es.cice.staticfragments.fragments.QuoteFragment;
 import es.cice.staticfragments.fragments.TitlesListFragment;
 
 public class MainActivity extends AppCompatActivity implements TitlesListFragment.TitlesListFragmetHostingActivity {
 
+    public static final String TAG = "MainActivity";
     private QuoteFragment qFragment;
+    private int orientation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,10 +22,31 @@ public class MainActivity extends AppCompatActivity implements TitlesListFragmen
         setContentView(R.layout.activity_main);
         FragmentManager fm=getSupportFragmentManager();
         qFragment= (QuoteFragment) fm.findFragmentById(R.id.quoteFragment);
+        orientation = getResources().getConfiguration().orientation;
+        Log.d(TAG, "onCreate() : "+orientation+" ]");
+
+
     }
 
     @Override
     public void showTitle(int index) {
-        qFragment.showTitle(index);
+
+        if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            if ( qFragment != null ) {
+                qFragment.showTitle(index);
+            }
+        } else {
+            Intent intent = new Intent(this, QuoteActivity.class);
+            intent.putExtra(QuoteActivity.QUOTE_INDEX, index);
+            startActivity(intent);
+        }
+
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        orientation = newConfig.orientation;
+        Log.d(TAG, "onConfigurationChanged[orientation: "+orientation+" ]");
     }
 }
--- StaticFragments/app/src/main/java/es/cice/staticfragments/QuoteActivity.java
+++ StaticFragments/app/src/main/java/es/cice/staticfragments/QuoteActivity.java
@@ -1,18 +1,28 @@
 package es.cice.staticfragments;
 
+import android.content.Intent;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import es.cice.staticfragments.fragments.QuoteFragment;
 
 public class QuoteActivity extends AppCompatActivity {
 
     public static final String QUOTE_INDEX="index";
-    private String[] quotes;
-    private String[] titles;
+    private QuoteFragment gFragment;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quote);
-        quotes=getResources().getStringArray(R.array.citas);
-        titles=getResources().getStringArray(R.array.titles);
+
+        gFragment = (QuoteFragment) getSupportFragmentManager().findFragmentById(R.id.quoteFragment2);
+
+        Intent intent = getIntent();
+        int index = intent.getIntExtra(QUOTE_INDEX, -1);
+        gFragment.showTitle(index);
+
     }
+
+
 }
